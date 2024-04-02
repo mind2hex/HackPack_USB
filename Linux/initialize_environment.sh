a62bedf758906692d8c28c941d894d4492c1d105
mind2hex <neodeus8@gmail.com>
2024-04-01 21:12:53 -0500

    profile added to initialization environment script

--- Linux/initialize_environment.sh
+++ Linux/initialize_environment.sh
@@ -1,25 +1,51 @@
 #!/bin/bash
 # script to downloads tools and create directory structure for working.
 
-echo -e "\n[!] \e[0;33m initial update && upgrade \e[0m"
+
+echo "---------------------------------------------------"
+echo -e "\n[!] \e[0;33m Initial update && upgrade \e[0m"
 sudo apt-get update && sudo apt-get upgrade
 
-echo -e "\n[!] \e[0;33m installing tools \e[0m" 
-tools=( \
-	docker.io docker-compose \
-	terminator emacs \
-	python3 python3-venv python3-pip \
-	git \
-	conky-all
+general_use_tools=(
+    docker.io docker-compose terminator conky-all neofetch
 )
-echo -e "--> ${tools[@]}"
-sudo apt install ${tools[@]}
+programming_tools=(
+    python3 python3-venv python3-pip emacs gdb git code
+)
+network_hacking_tools=(
+    nmap reaver wifite aircrack-ng  gnuradio wireshark gqrx-sdr
+)
+anonymizing_tools=(
+    tor torbrowser-launcher proxychains4 macchanger
+)
+
+echo "---------------------------------------------------"
+echo -e "\n[!] \e[0;33m Installing general use tools: ${general_use_tools[@]} \e[0m"
+sudo apt install ${general_use_tools[@]}
+
+echo "---------------------------------------------------"
+echo -e "\n[!] \e[0;33m Installing programming tools: ${programming_tools[@]} \e[0m"
+sudo apt install ${programming_tools[@]}
+
+echo "---------------------------------------------------"
+echo -e "\n[!] \e[0;33m Installing network hacking tools: ${programming_tools[@]} \e[0m"
+sudo apt install ${network_hacking_tools[@]}
+
+echo "---------------------------------------------------"
+echo -e "\n[!] \e[0;33m Installing anonymizing tools: ${anonymizing_tools[@]} \e[0m"
+sudo apt install ${anonymizing_tools[@]}
+
+## CONFIGURING SHELL PROFILE
+cp ./profile ~/.profile
+echo -e "\nsource ~/.profile" >> ~/.bashrc
+
+## INSTALLING EXPLOITDB
+sudo git clone https://gitlab.com/exploit-database/exploitdb.git /opt/exploit-database
 
 ## CONFIGURING CONKY
 echo -e "\n[!] \e[0;33m configuring conky \e[0m"
 mkdir -p ~/.config/conky/
 cp ./conky.conf ~/.config/conky/
-####################nnnn
 
 ## PROGRAMMING DIRECTORIES
 echo -e "\n[!] \e[0;33m creating programming directories in home dir\e[0m"
@@ -31,13 +57,12 @@ fi
 mkdir -p ${document_path}/dev/python
 mkdir -p ${document_path}/dev/c++
 mkdir -p ${document_path}/dev/c
-##########################
 
-## GIT CLONE TOOLS
+## CLONNING MY TOOLS
 echo -e "\n[!] \e[0;33m Cloning git tools\e[0m" 
 mkdir -p ${document_path}/github/my_tools
 git clone https://github.com/mind2hex/webToolkit ${document_path}/github/my_tools/webToolkit
 git clone https://github.com/mind2hex/NetRunner ${document_path}/github/my_tools/NetRunner
 git clone https://github.com/mind2hex/TCPCobra  ${document_path}/github/my_tools/TCPCobra
 git clone https://github.com/mind2hex/HackPack_USB  ${document_path}/github/my_tools/HackPack_USB
-###################
+
