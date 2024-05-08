b145b3aef78273804dc22020b11669fcdd406f1f
mind2hex <neodeus8@gmail.com>
2024-05-07 21:40:54 -0500

    initialize_environment.sh updated and bash_aliases bash_functions files created

--- Linux/environment_initialization/initialize_environment.sh
+++ Linux/environment_initialization/initialize_environment.sh
@@ -7,29 +7,28 @@ log_msg(){
     RED="\e[31m"
     YELLOW="\e[33m"
     BLUE="\e[34m"
+    END_COLOR="\e[0m"
 
     if [[ $2 == "ERROR" ]];then
         COLOR="${RED}"
     elif [[ $2 == "WARNING" ]];then
         COLOR="${YELLOW}"
     else
-        COLOR="${BLUE}"
+        COLOR="${END_COLOR}"
     fi
 
-    END_COLOR="\e[0m"
-
     echo -n "---------------------------------------------------"
-    echo -e "\n[!] ${COLOR} $1 ${END_COLOR}"
+    echo -e "\n[!] ${COLOR} $@ ${END_COLOR}"
 }
 
 # Function to detect distribution and package manager
 detect_package_manager(){
     if [[ -f /etc/debian_version ]];then
-        PACKAGE_MANAGER="apt"
-        PACKAGE_MANAGER_UPDATE="apt update"
-        PACKAGE_MANAGER_UPGRADE="apt upgrade -y"
-        PACKAGE_MANAGER_INSTALL="apt install -y"
-        PACKAGE_MANAGER_UNINSTALL="apt remove"
+        PACKAGE_MANAGER="apt-get"
+        PACKAGE_MANAGER_UPDATE="apt-get update"
+        PACKAGE_MANAGER_UPGRADE="apt-get upgrade -y"
+        PACKAGE_MANAGER_INSTALL="apt-get install -y"
+        PACKAGE_MANAGER_UNINSTALL="apt-get remove"
     elif [[ -f /etc/redhat-release  ]];then
         PACKAGE_MANAGER="yum"
         PACKAGE_MANAGER_UPDATE="yum update"
@@ -74,12 +73,9 @@ setup_directories(){
     pentest_dir="${HOME}/PENTEST"
     dev_dir="${pentest_dir}/dev/"
     github_dir="${pentest_dir}/github/"
-
-    conky_dir="${HOME}/.config/conky/"
     wordlist_dir="/usr/share/wordlists/"
 
     special_directories=(
-        ${conky_dir}
         ${dev_dir}
         ${github_dir}/{my_tools,cloned_tools}
         ${dev_dir}/{python,cc++,bash,web}
@@ -102,150 +98,230 @@ setup_directories(){
     done
 }
 
-# preconfiguration of some tools
-tools_preconfiguration(){
-    log_msg "Tools preconfiguration"
-
-    ## Kismet configuration
-    echo -e "\t== Kismet configuration =="
-    read -p "Do you want to remove all kismet previous installation (if exists) [Y/N]?" choice
-    if [[ "${choice}" = "Y" ]];then
-        for directories in "/usr/local/bin/kismet" "/usr/local/share/kismet" "/usr/local/etc/kismet";do
-            echo -e "\t- deleting ${directories}*"
-            sudo rm -rfv ${directories}*
-        done
-    fi
-    echo -e "\t- Adding kismet.list to /etc/apt/sources.list.d/kismet.list"
-    if ! [[ -f /etc/apt/sources.list.d/kismet.list ]];then
-        wget -O - https://www.kismetwireless.net/repos/kismet-release.gpg.key --quiet | gpg --dearmor | sudo tee /usr/share/keyrings/kismet-archive-keyring.gpg >/dev/null
-        echo 'deb [signed-by=/usr/share/keyrings/kismet-archive-keyring.gpg] https://www.kismetwireless.net/repos/apt/release/jammy jammy main' | sudo tee /etc/apt/sources.list.d/kismet.list >/dev/null
-    fi
-
-}
-
-# Install tools using package manager
-install_tools_via_pkg_manager(){
-    while [[ $# -gt 0 ]];do
-        declare -n tools=$1
-        log_msg "Installing $1"
-        for tool in ${tools[@]};do
-            echo -e -n "\t - Installing ${tool}..."
-            if [[ -z $( which ${tool} ) ]];then
-                sudo ${PACKAGE_MANAGER_INSTALL} ${tool}  >/dev/null 2>&1
-                if [[ $? -ne 0 ]];then
-                    echo -e "${RED} Error, unable to install.${END_COLOR}"
+# install tools using different resources (github, extern pages, package repositories, etc)
+install_tools(){
+    install_tools_with_pkg_manager(){
+        # $1 tools category
+        log_msg "Do you want to install ${YELLOW}$1${END_COLOR}?\n${tools[@]}"
+        read -p "[y/n]: " choice
+        if [[ "${choice}" = "y" ]];then
+            for tool in ${tools[@]}; do
+                echo -e -n "\t -> Installing $tool..."
+                sudo $PACKAGE_MANAGER_INSTALL $tool >/dev/null 2>&1
+                if [[ $? -eq 0 ]];then
+                    echo "Done"
                 else
-                    echo ""
+                    echo "Failed"
                 fi
+            done
+        fi
+    }
+
+    install_tools_from_github(){
+        for tool_url_path in ${github_tools[@]};do
+            
+            # tool name
+            tool=$( echo "$tool_url_path" | cut -d "@" -f 1 )
+
+            # tool url (github, gitlab, etc) should be a git reopsitory
+            url=$( echo "$tool_url_path"  | cut -d "@" -f 2 )
+
+            # path to where the tool will be cloned
+            path=$( echo "$tool_url_path" | cut -d "@" -f 3 )
+            
+            if [[ -e $path ]];then
+                log_msg "$tool already installed"
+                continue
+            fi
+
+            # installation for exploitdb
+            log_msg "installing $tool"
+            if [[ $tool = "exploitdb" ]];then
+                if ! [[ -d $path ]];then
+                    sudo git clone $url $path
+                    sudo ln -sf /opt/exploit-database/searchsploit /usr/local/bin/searchsploit
+                    cp -n /opt/exploit-database/.searchsploit_rc ~/
+                fi
+            elif [[ $tool = "linpeas.sh" ]];then
+                mkdir -p $path
+                curl -L "$url" > $path/linpeas.sh
+            elif [[ $tool = "rtl8812au" ]];then
+                git clone $url $path
+                # execute compilation process here...
             else
-                echo "Already installed"
+                git clone $url $path
             fi
         done
-        shift
-    done
-}
+    }
 
-# install tools cloning git repositories
-install_tools_via_git(){
+    # tools categories here, add or delete tools if needed
+    programming_tools=( python3 python3-venv python3-pip emacs git gdb )
+    container_tools=( docker.io docker-compose )
+    system_tools=( build-essential terminator conky-all neofetch htop tree )
+    network_analysis_tools=( wireshark netdiscover  )
+    network_security_tools=( aircrack-ng reaver bettercap )
+    radio_tools=( gnuradio gqrx-sdr rtl-sdr )
+    web_security_tools=( wfuzz sqlmap )
+    anonymity_tools=( tor torbrowser-launcher proxychains4 macchanger )
+    cracking_tools=( hydra john hashcat hashcat-nvidia )
+    malware_detection_tools=( rkhunter chkrootkit clamav )
+    github_tools=(
+        exploitdb@https://gitlab.com/exploit-database/exploitdb.git@/opt/exploit-database
+        webToolkit@https://github.com/mind2hex/webtoolkit@${github_dir}/my_tools/webToolkit
+        NetRunner@https://github.com/mind2hex/NetRunner@${github_dir}/my_tools/NetRuner
+        TCPCobra@https://github.com/mind2hex/TCPCobra@${github_dir}/my_tools/TCPCobra
+        Hackpack_usb@https://github.com/mind2hex/Hackpack_usb@${github_dir}/my_tools/Hackpack_usb
+        linpeas.sh@https://github.com/peass-ng/PEASS-ng/releases/latest/download/linpeas.sh@${github_dir}/cloned_tools/LinPeas/linpeas.sh
+        rtl8812au@https://github.com/aircrack-ng/rtl8812au.git@${github_dir}/cloned_tools/rtl8812au
+    )
 
-    # installing exploitdb and searchsploit
-    log_msg "Installing exploitdb from github"
-    if ! [[ -d /opt/exploit-database ]];then
-        sudo git clone https://gitlab.com/exploit-database/exploitdb.git /opt/exploit-database
-        sudo ln -sf /opt/exploit-database/searchsploit /usr/local/bin/searchsploit
-        cp -n /opt/exploit-database/.searchsploit_rc ~/
-    fi
-
-    # SecLists
-    log_msg "Installing SecLists wordlists to ${wordlist_dir}/SecLists"
-    if ! [[ -d ${wordlist_dir}/SecLists ]];then
-        sudo git clone https://github.com/danielmiessler/SecLists ${wordlist_dir}/SecLists
+    tools=( ${programming_tools[@]} )
+    install_tools_with_pkg_manager "programming_tools"
+    if [[ "${choice}" = "y" ]];then
+        # tools configuration here
+        echo -n ""
     fi
     
-    # Rockyou.txt
-    log_msg "Install rockyou.txt wordlist to ${wordlist_dir}/rockyou.txt"
-    if ! [[ -f ${wordlist_dir}/rockyou.txt ]];then
-        sudo wget 'https://github.com/brannondorsey/naive-hashcat/releases/download/data/rockyou.txt' -O ${wordlist_dir}/rockyou.txt
+    tools=( ${container_tools[@]} )    
+    install_tools_with_pkg_manager "container_tools"
+    if [[ "${choice}" = "y" ]];then
+        # tools configuration here
+        echo -n ""
     fi
 
-    # CLONNING MY TOOLS
-    my_tools=(
-        webToolkit
-        NetRunner
-        TCPCobra
-        Hackpack_usb
+    tools=( ${system_tools[@]} )
+    install_tools_with_pkg_manager "system_tools"    
+    if [[ "${choice}" = "y" ]];then
+        # conky configuration
+        mkdir -p ${HOME}/.config/conky/
+        cp ./conky.conf ${HOME}/.config/conky/
+
+        # neofetch configuration
+        if [[ -z $( grep -m 1 -o "neofetch" ~/.bashrc ) ]];then
+            echo -e "\nneofetch" >> ~/.bashrc
+        fi
+    fi
+
+    tools=( ${network_analysis_tools[@]} )
+    install_tools_with_pkg_manager "network_analysis_tools"
+    if [[ "${choice}" = "y" ]];then
+        # tools configuration here
+        echo -n ""
+    fi
+    
+    tools=( ${network_security_tools[@]} )
+    install_tools_with_pkg_manager "network_security_tools"
+    if [[ "${choice}" = "y" ]];then
+        # tools configuration here
+        echo -n ""
+    fi
+
+    tools=( ${radio_tools[@]} )
+    install_tools_with_pkg_manager "radio_tools"
+    if [[ "${choice}" = "y" ]];then
+        # tools configuration here
+        echo -n ""
+    fi
+    
+    tools=( ${web_security_tools[@]} )
+    install_tools_with_pkg_manager "web_security_tools"
+    if [[ "${choice}" = "y" ]];then
+        # tools configuration here
+        echo -n ""
+    fi
+    
+    tools=( ${anonymity_tools[@]} )
+    install_tools_with_pkg_manager "anonymity_tools"
+    if [[ "${choice}" = "y" ]];then
+        # tools configuration here
+        echo -n ""
+    fi
+    
+    tools=( ${cracking_tools[@]} )
+    install_tools_with_pkg_manager "cracking_tools"
+    if [[ "${choice}" = "y" ]];then
+        # tools configuration here
+        echo -n ""
+    fi
+    
+    tools=( ${malware_detection_tools[@]} )
+    install_tools_with_pkg_manager "malware_detection_tools"
+    if [[ "${choice}" = "y" ]];then
+        # tools configuration here
+        echo -n ""
+    fi
+
+    install_tools_from_github
+
+    # extern tools resources:
+    # code (visualstudiocode) https://code.visualstudio.com/
+    # nmap https://nmap.org/
+    # kismet https://www.kismetwireless.net/packages/
+    # gobuster https://github.com/OJ/gobuster
+    # bettercap https://www.bettercap.org/
+    # hackrf tools https://github.com/greatscottgadgets/hackrf.git
+}
+
+install_wordlists(){
+    wordlist=(
+        SecLists@https://github.com/danielmiessler/SecLists@${wordlist_dir}/SecLists
+        rockyou.txt@https://github.com/brannondorsey/naive-hashcat/releases/download/data/rockyou.txt@${wordlist_dir}/rockyou.txt
     )
-    for tool in ${my_tools[@]};do
-        log_msg "Installing ${tool} to ${github_dir}/my_tools/${tool}"
-        if ! [[ -d ${github_dir}/my_tools/${tool} ]];then
-            git clone https://github.com/mind2hex/${tool} ${github_dir}/my_tools/${tool}
+
+    for wordlist_url_path in ${wordlist[@]};do
+        # wordlist name
+        wordlist=$( echo "${wordlist_url_path}" | cut -d "@" -f 1 )
+
+        # url to the github wordlist
+        url=$( echo "${wordlist_url_path}" | cut -d "@" -f 2 )
+
+        # path to the destination of the wordlist
+        path=$( echo "${wordlist_url_path}" | cut -d "@" -f 3 )
+
+        # checking if wordlist is already installed
+        if [[ -f ${path} ]];then
+            continue
+        fi
+
+        log_msg "Installing $wordlist to $path"
+        if [[ $wordlist = "rockyou.txt" ]];then
+            sudo wget "$url" -O "$path"
+        else
+            sudo git clone $url $path
         fi
     done
-
-    #  ALFA Network AWUS036AC Driver installation for ubuntu
-    log_msg "Installing ALFA Network drivers"
-    read -p "Do you want to install ALFA Network AWUS036AC drivers? (Y/N) : " choice
-    if [[ "${choice}" = "Y" ]];then
-        system=$(uname -v | grep --ignore-case -E -o "(ubuntu|kali)" | cut -d " " -f 1)
-        if [[ $system = "Ubuntu" ]];then
-            if ! [[ -d ${github_dir}/cloned_tools/rtl8812au ]];then
-                git clone https://github.com/aircrack-ng/rtl8812au.git ${github_dir}/cloned_tools/rtl8812au
-            fi
-        fi
-    fi	
-
-    # LinPeas
-    log_msg "Installing LinPease"
-    mkdir -p ${github_dir}/cloned_tools/LinPeas/
-    curl -L https://github.com/peass-ng/PEASS-ng/releases/latest/download/linpeas.sh > ${github_dir}/cloned_tools/LinPeas/linpeas.sh
 }
-
-configure_tools(){
-    # CONFIGURING CONKY
-    cp ./conky.conf ${conky_dir}
-}
-
 
 additional_configurations(){
-    ## CONFIGURING SHELL PROFILE
-    foo=$(cat ~/.bashrc | grep)
-    cp ./profile ~/.profile
-    echo -e "\nsource ~/.profile" >> ~/.bashrc
+    log_msg "Copying ./bash_aliases to ~/.bash_aliases"
+    cp ./bash_aliases ~/.bash_aliases
+    if [[ -z $( grep "source .bash_aliases" ~/.bashrc ) ]];then
+        echo -e "\nsource ./bash_aliases" >> ~/.bashrc
+    fi
 
-    ## CONFIGURING /etc/hosts
+    log_msg "Copying ./bash_functions to ~/.bash_functions"
+    cp ./bash_functions ~/.bash_functions
+    if [[ -z $( grep -o "source .bash_functions" ~/.bashrc ) ]];then
+        echo -e "\nsource ./bash_functions" >> ~/.bashrc
+    fi
+
+    log_msg "Adding wifi pineapple address to /etc/hosts"
     wifi_pineapple_addr="172.16.42.1"
-    hosts=$(cat /etc/hosts | grep -o "${wifi_pineapple_addr}")
+    hosts=$( grep -o "${wifi_pineapple_addr}" /etc/hosts )
     if [[ -z "${hosts}" ]];then
 	    echo -e "172.16.42.1\twifi-pineapple.net\t# port 1471" | sudo tee -a /etc/hosts > /dev/null
     fi
 }
 
 main (){
-    general_tools=(
-	    docker.io docker-compose terminator conky-all neofetch
-    )
-    programming_tools=(
-	    python3 python3-venv python3-pip emacs gdb git code build-essential
-    )
-    network_tools=(
-	    nmap reaver wifite aircrack-ng  gnuradio wireshark gqrx-sdr netdiscover kismet bettercap
-    )
-    anonymizing_tools=(
-	    tor torbrowser-launcher proxychains4 macchanger
-    )
-    web_tools=(
-	    gobuster wfuzz sqlmap
-    )
-    security_tools=(
-	    rkhunter chkrootkit clamav hydra john hashcat
-    )
-
+    # request sudo rights password
+    sudo whoami > /dev/null
     detect_package_manager
-    #tools_preconfiguration  # before update_upgrade to reflect changes in sources
-    #update_upgrade    
-    #setup_directories
-    install_tools_via_pkg_manager "general_tools" "programming_tools" "network_tools" "anonymizing_tools" "web_tools" "security_tools"
-    #install_tools_via_git
+    update_upgrade    
+    setup_directories
+    install_tools
+    install_wordlists
+    additional_configurations
     exit 0
 }
 
